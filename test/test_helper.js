b7b63461f2abed6178ca89335fb599e4d5a7046d
Jenna Vuong <jennavuong@gmail.com>
2016-07-03 16:51:56 -0700

    add comments

--- test/test_helper.js
+++ test/test_helper.js
@@ -8,6 +8,8 @@ const win = doc.defaultView;
 global.document = doc;
 global.window = win;
 
+// so window properties can be used w/o window, which is what 
+// happens in a browser environment
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
     global[key] = window[key];
