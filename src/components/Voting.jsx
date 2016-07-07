0bea22a853bb03bc62485fb18a8ecf299d6ed2c6
Jenna Vuong <jennavuong@gmail.com>
2016-07-07 00:08:24 -0700

    once user already voted for a pair, don't let them vote again, show label on button

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -5,16 +5,18 @@ export default React.createClass({
   getPair: function() {
     return this.props.pair || [];
   },
-  isDisabled: function() {
+  isDisabled: function() { // once user already voted for pair, we shouldn't let them vote again
     return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
   },
   render: function() {
     return <div className="voting">
       {this.getPair().map(entry =>
-        <button key={entry}>
-                disabled={this.isDisabled()}
-                onClick={() => this.props.vote(entry)}>
+        <button key={entry}> disabled={this.isDisabled()} onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
+          { this.hasVotedFor(entry) ? <div className="label">Voted<div> : null }
         </button>
       )}
     </div>;
