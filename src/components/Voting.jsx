890c188de06bf684cbd3738c3204dd787c3e4d26
Jenna Vuong <jennavuong@gmail.com>
2016-07-08 16:15:06 -0700

    add test spec to disable button when user has voted

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -5,9 +5,7 @@ import Vote from './Vote';
 export default React.createClass({
   render: function() {
     return <div>
-      {this.props.winner ?
-        <Winner ref="winner" winner={this.props.winner} /> :
-        <Vote {...this.props} />}
+      { this.props.winner ? <Winner ref="winner" winner={this.props.winner} /> : <Vote {...this.props} /> }
     </div>;
   }
 });
