0e4537ee87ee0d8a491a1876b95b9e93f1d461c3
Jenna Vuong <jennavuong@gmail.com>
2016-07-08 16:55:31 -0700

    add react-router@2.0.0 and App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
-ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting" />, document.getElementById('app')
-);
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
+ReactDOM.render(<Voting pair={pair} />, document.getElementById('app'));
