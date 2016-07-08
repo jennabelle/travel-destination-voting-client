52a115b0f1ca4d546eab8b1ee6f3b4e9eea43b1d
Jenna Vuong <jennavuong@gmail.com>
2016-07-08 16:49:11 -0700

    add pure render mixin

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
-  render: function() {
-    return <div className="winner">
-      Winner is {this.props.winner}!
-    </div>;
-  }
+	mixins: [ PureRenderMixin ],
+	render: function() {
+	  	return <div className="winner">Winner is {this.props.winner}!</div>;
+	}
 });
