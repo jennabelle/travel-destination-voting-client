c5a9b1240c5429ec0435c61cfab8b560b54403ce
Jenna Vuong <jennavuong@gmail.com>
2016-07-09 10:34:56 -0700

    add unit test to results spec

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -25,4 +25,19 @@ describe('Results', () => {
     expect(days).to.contain('0');
   });
 
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocument(
+      <Results pair={pair}
+               tally={Map()}
+               next={next}/>
+    );
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+
+    expect(nextInvoked).to.equal(true);
+  });
+
 });
