1fe0ee2e53eda4ccef7bc35bf35d3d39ef3517f6
Jenna Vuong <jennavuong@gmail.com>
2016-07-12 17:10:44 -0700

    wire up Voting component to redux

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { connect } from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
-export default React.createClass({
-  mixins: [ PureRenderMixin ],
+const Voting = React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
-      { this.props.winner ? <Winner ref="winner" winner={this.props.winner} /> : <Vote {...this.props} /> }
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+connect(mapStateToProps)(Voting);
+
+export default Voting;
