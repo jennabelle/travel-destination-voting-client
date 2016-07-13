5aec0aad703cf4717dd81e97fdc85031df4abb18
Jenna Vuong <jennavuong@gmail.com>
2016-07-12 17:14:43 -0700

    export Voting component

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
-const Voting = React.createClass({
+export const Voting = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     return <div>
